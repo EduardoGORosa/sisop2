7bb7cff68e058eede9bc59c900dbbf21e5e6a570
jean.s.argoud <jean.s.argoud@gmail.com>
2025-05-19 01:09:17 -0300

    Voltando ao server fix

--- client/client.c
+++ client/client.c
@@ -1,138 +1,280 @@
 // client/client.c
 
+#include "../common/packet.h"
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <libgen.h>
+#include <limits.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <arpa/inet.h>
-#include <dirent.h>
+#include <sys/inotify.h>
 #include <sys/stat.h>
-#include "../common/packet.h"
+#include <unistd.h>
 
 #define CHUNK_SIZE MAX_PAYLOAD
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
+
+int sock_fd; // socket descriptor shared with sync thread
 
 int send_and_wait_ack(int s, packet_t *p) {
-    send_packet(s, p);
-    packet_t a;
-    return (recv_packet(s, &a) == 0 && a.type == PKT_ACK) ? 0 : -1;
+  send_packet(s, p);
+  packet_t a;
+  return (recv_packet(s, &a) == 0 && a.type == PKT_ACK) ? 0 : -1;
+}
+
+void *sync_thread(void *arg) {
+  int fd = inotify_init();
+  if (fd < 0) {
+    perror("inotify_init");
+    return NULL;
+  }
+
+  int wd = inotify_add_watch(fd, ".", IN_CREATE | IN_MODIFY | IN_DELETE);
+  if (wd < 0) {
+    perror("inotify_add_watch");
+    close(fd);
+    return NULL;
+  }
+
+  char buffer[EVENT_BUF_LEN];
+  while (1) {
+    int length = read(fd, buffer, EVENT_BUF_LEN);
+    if (length < 0)
+      break;
+
+    int i = 0;
+    while (i < length) {
+      struct inotify_event *evt = (struct inotify_event *)&buffer[i];
+      if (!(evt->mask & IN_ISDIR)) {
+        // Created or modified: upload
+        if (evt->mask & (IN_CREATE | IN_MODIFY)) {
+          char *name = evt->name;
+          // send upload request
+          packet_t req = {.type = PKT_UPLOAD_REQ,
+                          .seq_num = 1,
+                          .payload_size = (uint32_t)strlen(name) + 1};
+          memcpy(req.payload, name, req.payload_size);
+          if (send_and_wait_ack(sock_fd, &req) == 0) {
+            FILE *fp = fopen(name, "rb");
+            if (fp) {
+              uint32_t seq = 2;
+              char buf[CHUNK_SIZE];
+              size_t n;
+              while ((n = fread(buf, 1, CHUNK_SIZE, fp)) > 0) {
+                packet_t dp = {.type = PKT_UPLOAD_DATA,
+                               .seq_num = seq++,
+                               .payload_size = (uint32_t)n};
+                memcpy(dp.payload, buf, n);
+                send_and_wait_ack(sock_fd, &dp);
+              }
+              packet_t endp = {
+                  .type = PKT_UPLOAD_DATA, .seq_num = seq, .payload_size = 0};
+              send_packet(sock_fd, &endp);
+              fclose(fp);
+            }
+          }
+        }
+        // Deleted: tell server
+        else if (evt->mask & IN_DELETE) {
+          char *name = evt->name;
+          packet_t rq = {.type = PKT_DELETE_REQ,
+                         .seq_num = 1,
+                         .payload_size = (uint32_t)strlen(name) + 1};
+          memcpy(rq.payload, name, rq.payload_size);
+          send_packet(sock_fd, &rq);
+          packet_t resp;
+          recv_packet(sock_fd, &resp);
+        }
+      }
+      i += EVENT_SIZE + evt->len;
+    }
+  }
+
+  inotify_rm_watch(fd, wd);
+  close(fd);
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Uso: %s <user> <host> <port>\n", argv[0]);
-        return 1;
-    }
-    const char *user = argv[1];
-    const char *host = argv[2];
-    int port = atoi(argv[3]);
+  if (argc != 4) {
+    fprintf(stderr, "Uso: %s <user> <host> <port>\n", argv[0]);
+    return 1;
+  }
+  const char *user = argv[1];
+  const char *host = argv[2];
+  int port = atoi(argv[3]);
 
-    mkdir("sync_dir", 0755);
-    chdir("sync_dir");
+  mkdir("sync_dir", 0755);
+  chdir("sync_dir");
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in serv = { .sin_family = AF_INET, .sin_port = htons(port) };
-    inet_pton(AF_INET, host, &serv.sin_addr);
-    if (connect(sock, (void*)&serv, sizeof(serv)) < 0) {
-        perror("connect");
-        return 1;
-    }
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock_fd < 0) {
+    perror("socket");
+    return 1;
+  }
 
-    packet_t init = { .type = PKT_GET_SYNC_DIR, .seq_num = 1, .payload_size = (uint32_t)strlen(user)+1 };
-    memcpy(init.payload, user, init.payload_size);
-    send_packet(sock, &init);
-    packet_t ack;
-    recv_packet(sock, &ack);
+  struct sockaddr_in serv = {.sin_family = AF_INET, .sin_port = htons(port)};
+  inet_pton(AF_INET, host, &serv.sin_addr);
 
+  if (connect(sock_fd, (void *)&serv, sizeof(serv)) < 0) {
+    perror("connect");
+    close(sock_fd);
+    return 1;
+  }
+
+  // 1. Envia automaticamente o get_sync_dir
+  packet_t init = {.type = PKT_GET_SYNC_DIR,
+                   .seq_num = 1,
+                   .payload_size = (uint32_t)strlen(user) + 1};
+  memcpy(init.payload, user, init.payload_size);
+  if (send_and_wait_ack(sock_fd, &init) == 0) {
     printf("Sessão iniciada para '%s'\n", user);
-    printf("Comandos:\n"
-           " upload <file>\n"
-           " download <file>\n"
-           " delete <file>\n"
-           " list_server\n"
-           " list_client\n"
-           " exit\n");
+    printf("Diretório sync_dir inicializado no servidor.\n");
+  } else {
+    fprintf(stderr, "Falha ao inicializar sync_dir no servidor.\n");
+  }
 
-    char line[256];
-    while (printf("> "), fgets(line, sizeof(line), stdin)) {
-        char *cmd = strtok(line, " \t\n");
-        if (!cmd) continue;
+  printf("Comandos:\n"
+         " get_sync_dir\n"
+         " upload <file>\n"
+         " download <file>\n"
+         " delete <file>\n"
+         " list_server\n"
+         " list_client\n"
+         " exit\n");
 
-        if (strcmp(cmd, "upload") == 0) {
-            char *f = strtok(NULL, " \t\n");
-            if (f) {
-                packet_t rq = { .type = PKT_UPLOAD_REQ, .seq_num = 1, .payload_size = (uint32_t)strlen(f)+1 };
-                memcpy(rq.payload, f, rq.payload_size);
-                if (send_and_wait_ack(sock, &rq) == 0) {
-                    FILE *fp = fopen(f, "rb");
-                    uint32_t seq = 2;
-                    char buf[CHUNK_SIZE];
-                    size_t n;
-                    while ((n = fread(buf,1,CHUNK_SIZE,fp)) > 0) {
-                        packet_t dp = { .type = PKT_UPLOAD_DATA, .seq_num = seq++, .payload_size = (uint32_t)n };
-                        memcpy(dp.payload, buf, n);
-                        send_and_wait_ack(sock, &dp);
-                    }
-                    packet_t endp = { .type = PKT_UPLOAD_DATA, .seq_num = seq, .payload_size = 0 };
-                    send_packet(sock, &endp);
-                    fclose(fp);
-                }
+  // 3. Inicia thread de sync automático
+  pthread_t tid;
+  if (pthread_create(&tid, NULL, sync_thread, NULL) != 0) {
+    perror("pthread_create");
+  }
+
+  char line[256];
+  while (printf("> "), fgets(line, sizeof(line), stdin)) {
+    char *cmd = strtok(line, " \t\n");
+    if (!cmd)
+      continue;
+
+    if (strcmp(cmd, "get_sync_dir") == 0) {
+      packet_t rq = {.type = PKT_GET_SYNC_DIR,
+                     .seq_num = 1,
+                     .payload_size = (uint32_t)strlen(user) + 1};
+      memcpy(rq.payload, user, rq.payload_size);
+      if (send_and_wait_ack(sock_fd, &rq) == 0) {
+        printf("Diretório sync_dir (re)inicializado.\n");
+      }
+    } else if (strcmp(cmd, "upload") == 0) {
+      char *path = strtok(NULL, " \t\n");
+      if (path) {
+        char *filename = basename(path);
+        packet_t rq = {.type = PKT_UPLOAD_REQ,
+                       .seq_num = 1,
+                       .payload_size = (uint32_t)strlen(filename) + 1};
+        memcpy(rq.payload, filename, rq.payload_size);
+        if (send_and_wait_ack(sock_fd, &rq) == 0) {
+          FILE *fp = fopen(path, "rb");
+          if (!fp) {
+            perror("fopen");
+          } else {
+            uint32_t seq = 2;
+            char buf[CHUNK_SIZE];
+            size_t n;
+            while ((n = fread(buf, 1, CHUNK_SIZE, fp)) > 0) {
+              packet_t dp = {.type = PKT_UPLOAD_DATA,
+                             .seq_num = seq++,
+                             .payload_size = (uint32_t)n};
+              memcpy(dp.payload, buf, n);
+              send_and_wait_ack(sock_fd, &dp);
             }
+            packet_t endp = {
+                .type = PKT_UPLOAD_DATA, .seq_num = seq, .payload_size = 0};
+            send_packet(sock_fd, &endp);
+            fclose(fp);
+          }
         }
-        else if (strcmp(cmd, "download") == 0) {
-            char *f = strtok(NULL, " \t\n");
-            if (f) {
-                packet_t rq = { .type = PKT_DOWNLOAD_REQ, .seq_num = 1, .payload_size = (uint32_t)strlen(f)+1 };
-                memcpy(rq.payload, f, rq.payload_size);
-                send_packet(sock, &rq);
-                packet_t r; recv_packet(sock, &r);
-                FILE *fp = fopen(f, "wb");
-                while (1) {
-                    packet_t dp;
-                    recv_packet(sock, &dp);
-                    if (dp.payload_size == 0) break;
-                    fwrite(dp.payload,1,dp.payload_size,fp);
-                    packet_t ca = { .type = PKT_ACK, .seq_num = dp.seq_num, .payload_size = 0 };
-                    send_packet(sock, &ca);
-                }
-                fclose(fp);
-            }
+      }
+    } else if (strcmp(cmd, "download") == 0) {
+      char *f = strtok(NULL, " \t\n");
+      if (f) {
+        packet_t rq = {.type = PKT_DOWNLOAD_REQ,
+                       .seq_num = 1,
+                       .payload_size = (uint32_t)strlen(f) + 1};
+        memcpy(rq.payload, f, rq.payload_size);
+        send_packet(sock_fd, &rq);
+
+        packet_t r;
+        recv_packet(sock_fd, &r);
+        FILE *fp = fopen(f, "wb");
+        if (!fp) {
+          perror("fopen");
+          continue;
         }
-        else if (strcmp(cmd, "delete") == 0) {
-            char *f = strtok(NULL, " \t\n");
-            if (f) {
-                packet_t rq = { .type = PKT_DELETE_REQ, .seq_num = 1, .payload_size = (uint32_t)strlen(f)+1 };
-                memcpy(rq.payload, f, rq.payload_size);
-                send_packet(sock, &rq);
-                packet_t r; recv_packet(sock, &r);
-            }
-        }
-        else if (strcmp(cmd, "list_server") == 0) {
-            packet_t rq = { .type = PKT_LIST_SERVER_REQ, .seq_num = 1, .payload_size = 0 };
-            send_packet(sock, &rq);
-            packet_t res; recv_packet(sock, &res);
-            fwrite(res.payload,1,res.payload_size,stdout);
-        }
-        else if (strcmp(cmd, "list_client") == 0) {
-            struct stat st;
-            DIR *d = opendir(".");
-            struct dirent *e;
-            while ((e = readdir(d))) {
-                if (!strcmp(e->d_name,".") || !strcmp(e->d_name,"..")) continue;
-                stat(e->d_name, &st);
-                printf("%s\t%ld bytes\n", e->d_name, (long)st.st_size);
-            }
-            closedir(d);
-        }
-        else if (strcmp(cmd, "exit") == 0) {
+
+        while (1) {
+          packet_t dp;
+          recv_packet(sock_fd, &dp);
+          if (dp.payload_size == 0)
             break;
+          fwrite(dp.payload, 1, dp.payload_size, fp);
+          packet_t ca = {
+              .type = PKT_ACK, .seq_num = dp.seq_num, .payload_size = 0};
+          send_packet(sock_fd, &ca);
         }
-        else {
-            printf("Comando desconhecido\n");
-        }
-    }
+        fclose(fp);
+      }
+    } else if (strcmp(cmd, "delete") == 0) {
+      char *f = strtok(NULL, " \t\n");
+      if (f) {
+        packet_t rq = {.type = PKT_DELETE_REQ,
+                       .seq_num = 1,
+                       .payload_size = (uint32_t)strlen(f) + 1};
+        memcpy(rq.payload, f, rq.payload_size);
+        send_packet(sock_fd, &rq);
+        packet_t r;
+        recv_packet(sock_fd, &r);
+      }
+    } else if (strcmp(cmd, "list_server") == 0) {
+      packet_t rq = {
+          .type = PKT_LIST_SERVER_REQ, .seq_num = 1, .payload_size = 0};
+      send_packet(sock_fd, &rq);
+      packet_t res;
+      recv_packet(sock_fd, &res);
+      fwrite(res.payload, 1, res.payload_size, stdout);
+    } else if (strcmp(cmd, "list_client") == 0) {
+      struct stat st;
+      DIR *d = opendir(".");
+      struct dirent *e;
+      while ((e = readdir(d))) {
+        if (!strcmp(e->d_name, ".") || !strcmp(e->d_name, ".."))
+          continue;
+        if (stat(e->d_name, &st) < 0)
+          continue;
 
-    close(sock);
-    return 0;
+        char mtime[20], atime[20], ctime_buf[20];
+        struct tm *tm_info;
+
+        tm_info = localtime(&st.st_mtime);
+        strftime(mtime, sizeof(mtime), "%Y-%m-%d %H:%M:%S", tm_info);
+        tm_info = localtime(&st.st_atime);
+        strftime(atime, sizeof(atime), "%Y-%m-%d %H:%M:%S", tm_info);
+        tm_info = localtime(&st.st_ctime);
+        strftime(ctime_buf, sizeof(ctime_buf), "%Y-%m-%d %H:%M:%S", tm_info);
+
+        printf("%-20s %8ld bytes  mtime:%s  atime:%s  ctime:%s\n", e->d_name,
+               (long)st.st_size, mtime, atime, ctime_buf);
+      }
+      closedir(d);
+    } else if (strcmp(cmd, "exit") == 0) {
+      break;
+    } else {
+      printf("Comando desconhecido\n");
+    }
+  }
+
+  close(sock_fd);
+  return 0;
 }
 
--- server/server.c
+++ server/server.c
@@ -10,16 +10,17 @@
 #include <pthread.h>
 #include "../common/packet.h"
 
-#define SERVER_PORT   12345
-#define BACKLOG       10
-#define CHUNK_SIZE    MAX_PAYLOAD
-#define MAX_USER_LEN  256
+#define SERVER_PORT       12345
+#define BACKLOG           10
+#define CHUNK_SIZE        MAX_PAYLOAD
+#define MAX_USER_LEN      256
+#define MAX_USER_CONNS    10
 
 typedef struct UserSess {
-    char username[MAX_USER_LEN];
-    int  count;
-    int  conn_fd;
-    struct UserSess *next;
+    char     username[MAX_USER_LEN];
+    int      conn_fds[MAX_USER_CONNS];
+    int      conn_count;
+    struct   UserSess *next;
 } UserSess;
 
 static UserSess *sessions_head = NULL;
@@ -45,11 +46,20 @@ static UserSess *get_user_sess(const char *user) {
     }
     UserSess *u = calloc(1, sizeof(*u));
     strncpy(u->username, user, MAX_USER_LEN - 1);
-    u->count = 0;
-    u->conn_fd = -1;
+    u->conn_count = 0;
     u->next = sessions_head;
     sessions_head = u;
     return u;
+}
+
+// Envia um sync-event (mesmo payload) a todos os outros fd do usuário
+static void broadcast_sync(UserSess *us, int origin_fd, packet_t *evt) {
+    for (int i = 0; i < us->conn_count; i++) {
+        int fd = us->conn_fds[i];
+        if (fd != origin_fd) {
+            send_packet(fd, evt);
+        }
+    }
 }
 
 void *handle_client(void *arg) {
@@ -63,7 +73,7 @@ void *handle_client(void *arg) {
         return NULL;
     }
 
-    // Copy user safely
+    // copia usuário
     char user[MAX_USER_LEN];
     size_t ulen = pkt.payload_size < (MAX_USER_LEN - 1)
                    ? pkt.payload_size
@@ -71,66 +81,88 @@ void *handle_client(void *arg) {
     memcpy(user, pkt.payload, ulen);
     user[ulen] = '\0';
 
-    // 2) Session control
+    // 2) controla sessões
     pthread_mutex_lock(&sessions_mutex);
     UserSess *us = get_user_sess(user);
-    if (us->count >= 2) {
+    if (us->conn_count >= MAX_USER_CONNS) {
         pthread_mutex_unlock(&sessions_mutex);
         packet_t nack = { .type = PKT_NACK, .seq_num = pkt.seq_num, .payload_size = 0 };
         send_packet(conn, &nack);
         close(conn);
         return NULL;
     }
-    us->count++;
-    us->conn_fd = conn;
+    us->conn_fds[us->conn_count++] = conn;
     pthread_mutex_unlock(&sessions_mutex);
 
-    // 3) Initial ACK
-    packet_t ack = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
-    send_packet(conn, &ack);
+    // 3) ack inicial
+    packet_t ack0 = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
+    send_packet(conn, &ack0);
     printf("[+] Sessão iniciada para '%s' (fd=%d) total=%d\n",
-           user, conn, us->count);
+           user, conn, us->conn_count);
 
-    // 4) Prepare directory
+    // 4) prepara diretório
     char base[512];
     snprintf(base, sizeof(base), "storage/%s/sync_dir", user);
     mkdir_p("storage", 0755);
     mkdir_p(base, 0755);
 
-    // 5) Command loop
+    // 5) loop de comandos
     while (recv_packet(conn, &pkt) == 0) {
-        if (pkt.type == PKT_SYNC_EVENT) {
-            continue;
+        // agora vamos tratar TUDO via switch, inclusive PKT_SYNC_EVENT
+        // extrai nome de arquivo (quando houver)
+        char fn[CHUNK_SIZE+1] = {0};
+        if (pkt.payload_size > 0) {
+            size_t flen = pkt.payload_size < CHUNK_SIZE ? pkt.payload_size : CHUNK_SIZE;
+            memcpy(fn, pkt.payload, flen);
+            fn[flen] = '\0';
         }
-
-        // Extract filename safely
-        size_t flen = pkt.payload_size < CHUNK_SIZE ? pkt.payload_size : CHUNK_SIZE;
-        char fn[CHUNK_SIZE+1];
-        memcpy(fn, pkt.payload, flen);
-        fn[flen] = '\0';
-
         char path[512];
         snprintf(path, sizeof(path), "%s/%s", base, fn);
 
         switch (pkt.type) {
+            case PKT_SYNC_EVENT: {
+                // ACK do evento recebido
+                packet_t r = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
+                send_packet(conn, &r);
+
+                printf("[*] PKT_SYNC_EVENT recebido para '%s'\n", fn);
+                // simplesmente retransmite o mesmo evento para os outros dispositivos
+                broadcast_sync(us, conn, &pkt);
+                break;
+            }
+
             case PKT_UPLOAD_REQ: {
                 printf("[*] Upload: %s\n", fn);
                 FILE *f = fopen(path, "wb");
-                packet_t r = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
-                send_packet(conn, &r);
+                // ACK do req
+                packet_t r1 = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
+                send_packet(conn, &r1);
+
+                // recebe chunks
                 while (recv_packet(conn, &pkt) == 0 && pkt.payload_size > 0) {
                     fwrite(pkt.payload, 1, pkt.payload_size, f);
                     packet_t ca = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
                     send_packet(conn, &ca);
                 }
                 fclose(f);
+
+                // propaga evento de upload para outros devices
+                packet_t evt = {
+                    .type = PKT_SYNC_EVENT,
+                    .seq_num = 0,
+                    .payload_size = (uint32_t)strlen(fn) + 1
+                };
+                memcpy(evt.payload, fn, evt.payload_size);
+                broadcast_sync(us, conn, &evt);
                 break;
             }
+
             case PKT_DOWNLOAD_REQ: {
                 printf("[*] Download: %s\n", fn);
                 FILE *f = fopen(path, "rb");
-                packet_t r = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
-                send_packet(conn, &r);
+                packet_t r2 = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
+                send_packet(conn, &r2);
+
                 uint32_t seq = 1;
                 while (!feof(f)) {
                     size_t n = fread(pkt.payload, 1, CHUNK_SIZE, f);
@@ -138,20 +170,32 @@ void *handle_client(void *arg) {
                     pkt.seq_num = seq++;
                     pkt.payload_size = (uint32_t)n;
                     send_packet(conn, &pkt);
-                    recv_packet(conn, &r);
+                    recv_packet(conn, &r2);
                 }
+                // finaliza
                 pkt.payload_size = 0;
                 send_packet(conn, &pkt);
                 fclose(f);
                 break;
             }
+
             case PKT_DELETE_REQ: {
                 printf("[*] Delete: %s\n", fn);
                 remove(path);
-                packet_t r2 = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
-                send_packet(conn, &r2);
+                packet_t r3 = { .type = PKT_ACK, .seq_num = pkt.seq_num, .payload_size = 0 };
+                send_packet(conn, &r3);
+
+                // propaga evento de delete para outros devices
+                packet_t evt2 = {
+                    .type = PKT_SYNC_EVENT,
+                    .seq_num = 0,
+                    .payload_size = (uint32_t)strlen(fn) + 1
+                };
+                memcpy(evt2.payload, fn, evt2.payload_size);
+                broadcast_sync(us, conn, &evt2);
                 break;
             }
+
             case PKT_LIST_SERVER_REQ: {
                 DIR *d = opendir(base);
                 struct dirent *e;
@@ -182,19 +226,28 @@ void *handle_client(void *arg) {
                 send_packet(conn, &res);
                 break;
             }
+
             default:
+                // outros tipos são ignorados
                 break;
         }
     }
 
-    // End session
+    // encerra sessão
     pthread_mutex_lock(&sessions_mutex);
-    us->count--;
-    if (us->conn_fd == conn) us->conn_fd = -1;
+    // remove this conn_fd do array
+    for (int i = 0; i < us->conn_count; i++) {
+        if (us->conn_fds[i] == conn) {
+            // move o último para cá
+            us->conn_fds[i] = us->conn_fds[--us->conn_count];
+            break;
+        }
+    }
     pthread_mutex_unlock(&sessions_mutex);
+
     close(conn);
     printf("[-] Sessão encerrada para '%s' (fd=%d) total=%d\n",
-           user, conn, us->count);
+           user, conn, us->conn_count);
     return NULL;
 }
 
@@ -205,12 +258,12 @@ int main(void) {
     if (listen_fd < 0) { perror("socket"); exit(1); }
 
     struct sockaddr_in addr = {
-        .sin_family = AF_INET,
+        .sin_family      = AF_INET,
         .sin_addr.s_addr = INADDR_ANY,
-        .sin_port = htons(SERVER_PORT)
+        .sin_port        = htons(SERVER_PORT)
     };
     if (bind(listen_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) { perror("bind"); exit(1); }
-    if (listen(listen_fd, BACKLOG) < 0) { perror("listen"); exit(1); }
+    if (listen(listen_fd, BACKLOG) < 0)    { perror("listen"); exit(1); }
     printf("Server listening on port %d\n", SERVER_PORT);
 
     while (1) {
